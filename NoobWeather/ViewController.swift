ace895d73e11f1c4c7497b3a8f38f4e508d79c59
Brad Booysen <brad.booysen@gmail.com>
2019-01-22 06:26:27 +1300

    Get device location

--- NoobWeather/ViewController.swift
+++ NoobWeather/ViewController.swift
@@ -7,21 +7,42 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    var latitude = 0.0
+    var longitude = 0.0
 
     @IBOutlet weak var weatherDescription: UILabel!
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var tempRange: UILabel!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+
     }
 
     @IBAction func refreshButton(_ sender: Any) {
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        let userlocation:CLLocationCoordinate2D = manager.location!.coordinate
+        latitude = userlocation.latitude
+        longitude = userlocation.longitude
+        print(latitude)
+        print(longitude)
     }
     
 }
