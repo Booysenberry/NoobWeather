950c694bfc0147fc71df041a197d330ce65b49cd
Brad Booysen <brad.booysen@gmail.com>
2019-01-21 16:36:59 +1300

    Initial Commit

---
+++ NoobWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoobWeather
+//
+//  Created by Brad Booysen on 21/01/19.
+//  Copyright © 2019 Brad Booysen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
