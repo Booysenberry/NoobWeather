e6bffb88c6c394f45cf301b48d8c4482410a22fb
Brad Booysen <brad.booysen@gmail.com>
2019-01-23 15:35:26 +1300

    Setup api call using URLSession

--- NoobWeather/ViewController.swift
+++ NoobWeather/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var locationManager = CLLocationManager()
     var latitude = 0.0
     var longitude = 0.0
-
+    
     @IBOutlet weak var weatherDescription: UILabel!
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var tempRange: UILabel!
@@ -30,10 +30,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if CLLocationManager.locationServicesEnabled() {
             locationManager.startUpdatingLocation()
         }
-
     }
-
+    
     @IBAction func refreshButton(_ sender: Any) {
+        getWeatherData()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -41,9 +41,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let userlocation:CLLocationCoordinate2D = manager.location!.coordinate
         latitude = userlocation.latitude
         longitude = userlocation.longitude
-        print(latitude)
-        print(longitude)
+//        print(latitude)
+//        print(longitude)
+        getWeatherData()
     }
     
+    func getWeatherData() {
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=5c1013a7da44573668f4d581562109dd")!
+        let task = session.dataTask(with: url) { data, response, error in
+            
+            // Check for errors
+            guard error == nil else {
+                print ("error: \(error!)")
+                return
+            }
+            // Check that data has been returned
+            guard let content = data else {
+                print("No data")
+                return
+            }
+            // Serialise data into Dictionary [String : Any]
+            guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
+                print("Not containing JSON")
+                return
+            }
+            print("Response is \n \(json)")
+            // update UI using the response here
+        }
+        // execute the HTTP request
+        task.resume()
+    }
 }
 
